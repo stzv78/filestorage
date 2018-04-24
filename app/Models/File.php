1b80a262b6cde446f0bd94b7b4dbdeced7a7beb8
stzv78 <stzv78@mail.ru>
2018-04-24 22:55:39 +0400

    Made File model

---
+++ app/Models/File.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class File extends Model
+{
+    protected $fillable = [
+        'id',
+        'description',
+        'hash_file', 
+        'hash_user',
+    ];
+}
